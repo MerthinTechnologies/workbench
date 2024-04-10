c73de3cf1e6f7361b9eebaeb69317fc03e82986d
gprotel <luis@merthin.com>
2024-04-10 21:30:04 +0000

    more installation steps

--- workbench.install.sh
+++ workbench.install.sh
@@ -35,15 +35,16 @@ mkdir -p $CFGDIR
 mkdir -p $BINDIR
 sleep 1
 echo "Downloading components"
-curl -sLo $CFGDIR/workbench.functions-1.sh https://raw.githubusercontent.com/MerthinTechnologies/workbench/master/dist/workbench.functions.sh
+curl -sLo $CFGDIR/workbench.functions.sh https://raw.githubusercontent.com/MerthinTechnologies/workbench/master/dist/workbench.functions.sh
 curl -sLo $BINDIR/workbench https://raw.githubusercontent.com/MerthinTechnologies/workbench/master/dist/workbench
 chmod 755 $BINDIR/workbench
 echo ""
 echo "Merthin's workbench installed."
 echo ""
 echo "Please include $BINDIR to your exec PATH. At this to your shell rc file (i.e. .bashrc):"
+echo ""
 echo "export PATH=$PATH:$BINDIR"
-
+echo ""
 
 
 
